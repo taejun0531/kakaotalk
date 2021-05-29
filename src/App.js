a801007509261429d26697ac8851bc94526b233c
taejun <taejun0531@gmail.com>
2021-05-29 18:43:26 +0900

    kakaotalk

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { inject, observer } from 'mobx-react';
 
 const name = ["윤태준", "승화이", "주뽕"];
 const src = ["./img/KakaoTalk_user_img.jpg", "./img/user_img_psh.jpg", "./img/user_img_jjh.jpg"];
-const subtitle = ["03년생 입니다.", "나 완전 체했어.", "수능생이였다."];
+const subtitle = ["안녕하세요.", "나 완전 체했어.", "수능생이였다."];
 const content = [[""], ["나 아까 라면 먹었는데", "체했어.ㅋㅋ"], ["드디어 수능 끝났다.", "등급은 물어보지 마라."]];
 
 @inject('userStore')
